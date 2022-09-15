be3b3b04da94cac5e1c28e81f153dc55fe5aa108
devfemibadmus <devfemibadmus@gmail.com>
2022-09-15 14:14:21 +0100

    init

---
+++ akogunakinniyi/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from home import views
+
+urlpatterns = [
+    path('', include('home.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns = urlpatterns + [
+    re_path(r'.*', views.career, name="Home"),
+]
